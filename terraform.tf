7d1edd695f4746bf2fbb2d9579a1a4216bb6b88a
sysadmin <sysadmin@terraform.is404notfound.fr>
2022-01-10 16:05:51 +0100

    Update

--- terraform.tf
+++ terraform.tf
@@ -6,3 +6,16 @@ terraform {
   }
   required_version = ">= 0.13"
 }
+
+provider "scaleway" {
+  zone = "fr-par-1"
+  region = "fr-par"
+}
+
+resource "scaleway_instance_ip" "public_ip" {
+}
+
+resource "scaleway_vpc_private_network" "pn_priv" {
+    name = "Nextcloud_EPSI"
+    tags = ["Nextcloud"]
+}
